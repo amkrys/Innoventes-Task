bb8e1bd5d943c0e38b32e9a27c274f42947d9659
amkrys <amkrys007@gmail.com>
2024-06-10 21:14:15 +0530

    Added required string validations

--- app/src/main/java/com/innoventus/task/util/StringExt.kt
+++ app/src/main/java/com/innoventus/task/util/StringExt.kt
@@ -15,8 +15,28 @@ fun String.isValid(): Boolean {
     return stringNotNull() && trim().stringNotEmpty()
 }
 
-fun String.isValidPan(): Boolean {
-    val pattern: Pattern = Pattern.compile("[A-Z]{5}[0-9]{4}[A-Z]")
-    val matcher: Matcher = pattern.matcher(this)
-    return length <= 10 && matcher.matches()
+fun String?.isValidPan(): Boolean {
+    return this?.let {
+        val pattern: Pattern = Pattern.compile("[A-Z]{5}[0-9]{4}[A-Z]")
+        val matcher: Matcher = pattern.matcher(this)
+        return@let length <= 10 && matcher.matches()
+    } == true
+}
+
+fun String?.isValidDay(): Boolean {
+    return this?.let {
+        return@let this.isValid() && this.toInt() in 1..31
+    } == true
+}
+
+fun String?.isValidMonth(): Boolean {
+    return this?.let {
+        return@let this.isValid() && this.toInt() in 1 .. 12
+    } == true
+}
+
+fun String?.isValidYear(): Boolean {
+    return this?.let {
+        return@let this.isValid() && this.toInt() in 1990..2024
+    } == true
 }
